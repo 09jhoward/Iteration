5735fa781263580258aa284978375ad64a2c42eb
Jessica Howard <45182@longroad.ac.uk>
2014-10-22 15:01:52 +0100

    class work

---
+++ task 1 revision exercies.py
@@ -0,0 +1,13 @@
+#Jess Howard
+#22/10/12
+#task exercises
+
+#task 1 (revision exercises)
+for count in range(10):
+    print("Hello World")
+
+
+#task 2 (revision exercises)
+message= input("Please enter your message:")
+number= int(input("Please enter how many times you would like to repeat your message:")
+        
---
+++ task 2 revision exercies.py
@@ -0,0 +1,5 @@
+#task 2 (revision exercises)
+message= input("Please enter your message:")
+number= int(input("Please enter how many times you would like to repeat your message:"))
+for count in range(number):
+    print(message)
